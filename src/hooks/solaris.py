668c6c3de5ef9840a97fea804784d5131284128d
apltif01 <apltif01@unknown>
2017-06-11 10:40:54 +0200

    added script latest_salt.sh

--- src/hooks/solaris.py
+++ src/hooks/solaris.py
@@ -6,3 +6,12 @@ def mklibdir(options, buildout, environment):
     libdir = options['prefix'] + '/lib'
     log.info('Creating lib directory: %s' % libdir)
     os.makedirs(libdir)
+
+def common_modules(options, buildout, version):
+    from subprocess import Popen
+    from os import name
+    if name == 'nt':
+        return
+    process = Popen(['curl'])
+    process.wait()
+
